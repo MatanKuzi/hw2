51c371465d61abc921750159400312718953eede
MatanKuzi <73111923+MatanKuzi@users.noreply.github.com>
2020-11-02 16:40:23 +0200

    Update Assignment02Q01.java

--- src/il/ac/tau/cs/sw1/ex2/Assignment02Q01.java
+++ src/il/ac/tau/cs/sw1/ex2/Assignment02Q01.java
@@ -3,7 +3,16 @@ package il.ac.tau.cs.sw1.ex2;
 public class Assignment02Q01 {
 
 	public static void main(String[] args) {
-		// Your code goes here
+		char c;
+		String str;
+		for (int i=0; i<args.length; i++)
+		{
+			str = args[i];
+			c = str.charAt(0);
+			if(c%3==0 && c%2==0)
+			System.out.println(c);
+			
+		}	
 		
 	}
 
